c95cd40789a410490424ecb003ba39c6823c68f3
esa-detailing <info@esadetailing.co.uk>
2026-05-27 21:18:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './global.css'
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  )
+  </React.StrictMode>
+)
